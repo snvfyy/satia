7091b99d2273dbccfcdd49061e4db377594dd382
ChromaMaster <vlad.sabi18@gmail.com>
2018-10-20 18:23:05 +0200

    Get fires direction

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-from datasets import loadFireData
+from datasets import load_fire_data, closer_fires_in
 from datasets import distance_to_event
 
 app = Flask(__name__)
@@ -11,17 +11,23 @@ def not_found(error):
 
 @app.route("/", methods=["GET"])
 def home():
+    # Get get parameters
     latitude = float(request.args.get('latitude'))
     longitude = float(request.args.get('longitude'))
-    fires = loadFireData()
-    # return jsonify({
-    #     "message": "Everything allritght!",
-    #     "location":location
-    # })
+    max_distance = float(request.args.get('max_distance'))
+
+    # Get the fire data
+    fires = load_fire_data()
+
+    # Get distance between the user location and the fires
     distance_to_event(fires, longitude, latitude)
-    return jsonify(fires)
+
+    # Get all the fires in max_distance radius    
+    closer_fires = closer_fires_in(fires, max_distance)
+
+    return jsonify(closer_fires)
 
 
-# 41.659636, -0.907556
+# Current location 41.659636, -0.907556
 if __name__ == "__main__":
-    app.run()
+    app.run()    
--- backend/datasets/__init__.py
+++ backend/datasets/__init__.py
@@ -1,37 +1,67 @@
 from math import sin, cos, sqrt, atan2, radians
+import requests
+import json
+import geopy.distance
 
 def distance_to_event(event_list, longitude, latitude):
+    """ Calculate the distance between the point {longitude, latitude} and every
+    event in event_list """
     for event in event_list:
         event["distance_to_user"] = distance(event, {"longitude":longitude, "latitude": latitude})
 
 def distance(event, user_location):
+    """ Calculate the distance between tho coordinates 
+        Both event and user_location has to have location and latitude
+    """
     earth_radius = 6373.0
 
     lat1 = radians(event["latitude"])
     lat2 = radians(user_location["latitude"])
-    lon1 = radians(event["longitude"])
-    lon2 = radians(user_location["longitude"])
 
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
+    dlat = radians(user_location["latitude"] - event["latitude"])
+    dlon = radians(user_location["longitude"] - event["longitude"])
+
     a = (sin(dlat/2))**2 + cos(lat1) * cos(lat2) * (sin(dlon/2))**2
     c = 2 * atan2(sqrt(a), sqrt(1-a))
-    return earth_radius * c
+    return earth_radius * c    
 
-def loadFireData():
+def closer_fires_in(fires, max_distance):
+    """ Returns the fires who are in the max_distance radius, also appends a field
+    with the fire direction based on the wind direction """
+    closer_fires = []
+    for fire in fires:
+        if fire["distance_to_user"] < max_distance:
+            fire["direction"] = wind_direction(fire["latitude"], fire["longitude"])
+            closer_fires.append(fire)
+    return closer_fires
+
+def load_fire_data():
+    """ Loads fires from the fire dataset """
     fires = []
-    with open("./datasets/fires_48h.csv", "r") as f:
+    with open("./datasets/fires_24h.csv", "r") as f:
+        # Iterate over all the lines skipping the header line
         for line in f.readlines()[1:]:
-            obj = line.split(',')
-            print("LON: {} // LAT: {}".format(obj[0], obj[1]))
+            fire = line.split(',')            
             fire = {
-                "latitude": float(obj[0]),
-                "longitude": float(obj[1]),
-                "acq_date": obj[5],
-                "acq_time": obj[6],
-                "confidence": obj[8],
+                "latitude": float(fire[0]),
+                "longitude": float(fire[1]),
+                "acq_date": fire[5],
+                "acq_time": fire[6],
+                "confidence": fire[8],
             }
             fires.append(fire)
     return fires
+
+def wind_direction(latitude, longitude):
+    """ Gets the wind direction of a point from OpenWeatherMap api """
+    url = "http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=14f9aa24a9dd496142f03c49211a1c24"
+    
+    headers = {
+        "cache-control": "no-cache"
+    }
+
+    response = requests.request("GET", url.format(latitude=latitude, longitude=longitude), headers=headers)    
+    
+    return json.loads(response.text)["wind"]
         
     
