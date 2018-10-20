560a1bef180f33815f12f456e2595775a7f63b7d
darknnesilver <sebastianciocarlan@gmail.com>
2018-10-20 11:36:35 -0500

    gps update

---
+++ frontend/js/main.js
@@ -0,0 +1,12 @@
+	var location;
+				function getLocation() {
+				    if (navigator.geolocation) {
+				        navigator.geolocation.getCurrentPosition(showPosition);
+				    } else {
+				       console.log ("Geolocation is not supported by this browser.");
+				    }
+				}
+				function showPosition(position) {
+				    console.log("Latitude: " + position.coords.latitude +
+				    "<br>Longitude: " + position.coords.longitude);
+				}
